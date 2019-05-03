b893b8af42c07a52be1b22574aff7b92e4f2beda
Stoyan <nevenov@abv.bg>
2019-05-03 18:00:30 +0300

    user CRUD completed

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-sm-3">
+
+            <img src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400'}}" alt="" class="img-responsive img-rounded">
+
+        </div>
+
+
+        <div class="col-sm-9">
+
+            <h1>Edit User</h1>
+
+
+            {!! Form::model($user, ['method'=>'PATCH', 'action'=>['AdminUsersController@update', $user->id], 'files'=>true]) !!}
+
+            <div class="form-group">
+                {!! Form::label('name', 'Name:') !!}
+                {!! Form::text('name', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('email', 'Email:') !!}
+                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id', 'Role:') !!}
+                {!! Form::select('role_id', $roles, null, ['class'=>'form-control']) !!}
+            </div>
+
+
+            <div class="form-group">
+                {!! Form::label('is_active', 'Status:') !!}
+                {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), null , ['class'=>'form-control']) !!}
+            </div>
+
+
+            <div class="form-group">
+                {!! Form::label('file', 'Image:') !!}
+                {!! Form::file('file', null, ['class'=>'form-control']) !!}
+            </div>
+
+
+            <div class="form-group">
+                {!! Form::label('password', 'Password:') !!}
+                {!! Form::password('password', ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Update User', ['class'=>'btn btn-primary col-sm-4']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+
+
+            {!! Form::open(['method'=>'DELETE', 'action'=>['AdminUsersController@destroy', $user->id]]) !!}
+
+                <div class="form-group">
+                    {!! Form::submit('Delete user', ['class'=>'btn btn-danger col-sm-4 col-md-offset-4']) !!}
+                </div>
+
+            {!! Form::close() !!}
+
+
+        </div>
+
+    </div>
+
+
+    <div class="row">
+        @include('includes.form_errors')
+    </div>
+
+
+
+@endsection
