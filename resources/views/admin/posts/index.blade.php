00eac4cf480c3273c5217f694774dca94c82f792
Stoyan <nevenov@abv.bg>
2019-05-09 17:08:58 +0300

    able to create posts with dynamic categories

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -46,7 +46,7 @@
           <td>{{$post->id}}</td>
           <td><img height="50" src="{{$post->photo ? $post->photo->file : 'https://placehold.it/100'}}" alt=""></td>
           <td>{{$post->user->name}}</td>
-          <td>{{$post->category_id}}</td>
+          <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
           <td>{{$post->title}}</td>
           <td>{{$post->body}}</td>
           <td>{{$post->created_at->diffForHumans()}}</td>
